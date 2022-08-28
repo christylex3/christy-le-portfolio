04c1aa8e4430245e647506fac04ad8e99a33390b
Christy Le <christylex3@gmail.com>
2022-08-27 20:51:56 -0400

    Fixed target='_blank' issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,31 +10,24 @@ const Footer = () => {
 		<footer id="contact-info" class="flex flex-row justify-evenly">
 			<a href="mailto: christylex3@gmail.com">
 				{/* Credits: <a target="_blank" href="https://icons8.com/icon/37246/gmail-logo">Gmail Logo</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
-				<img
-					class="icon"
-					src={gmail}
-					alt="Gmail Logo icon by Icons8"
-				/>
+				<img class="icon" src={gmail} alt="Gmail Logo icon by Icons8" />
 			</a>
 			<a href="tel:770-843-6811">
 				{/* Credits: <a target="_blank" href="https://icons8.com/icon/13663/google-mobile">Google Mobile</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
-				<img
-					class="icon"
-					src={phone}
-					alt="phone"
-				/>
+				<img class="icon" src={phone} alt="phone" />
 			</a>
-			<a href="https://github.com/christylex3" target="_blank">
+			<a
+				href="https://github.com/christylex3"
+				target="_blank"
+				rel="noreferrer"
+			>
 				{/* Credits: <a target="_blank" href="https://icons8.com/icon/118553/github">GitHub</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
-				<img
-					class="icon"
-					src={gitHub}
-					alt="GitHub icon by Icons8"
-				/>
+				<img class="icon" src={gitHub} alt="GitHub icon by Icons8" />
 			</a>
 			<a
 				href="https://www.linkedin.com/in/christy-le-316960209/"
 				target="_blank"
+				rel="noreferrer"
 			>
 				{/* Credits: <a target="_blank" href="https://icons8.com/icon/114445/linkedin-circled">LinkedIn Circled</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */}
 				<img
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
 
 const Project = () => {
-    return (
-        <div>
-            <h2>WORK</h2>
-            <section className="project-gallery flex justify-between flex-col lg:flex-row">
-                <article className="project-card">
-                    <a href="https://mike-gonz0.github.io/sadLibs/" target="_blank">
-                        <h3>sadLibs</h3>
-                        <img src="../test-images/sadLibs.jpg" alt="sadLibs" />
-                    </a>
-                </article>
-                <article className="project-card">
-                    <a href="https://salty-island-34746.herokuapp.com/" target="_blank">
-                        <h3>Recipe Finder</h3>
-                        <img src="../test-images/recipefinder.png" alt="recipefinder" />  
-                    </a>
-                </article>
-            </section>
-        </div>
-    );
+	return (
+		<div>
+			<h2>WORK</h2>
+			<section className="project-gallery flex justify-between flex-col lg:flex-row">
+				<article className="project-card">
+					<a
+						href="https://mike-gonz0.github.io/sadLibs/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<h3>sadLibs</h3>
+						<img src="../test-images/sadLibs.jpg" alt="sadLibs" />
+					</a>
+				</article>
+				<article className="project-card">
+					<a
+						href="https://salty-island-34746.herokuapp.com/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<h3>Recipe Finder</h3>
+						<img
+							src="../test-images/recipefinder.png"
+							alt="recipefinder"
+						/>
+					</a>
+				</article>
+			</section>
+		</div>
+	);
 };
 
 export default Project;
