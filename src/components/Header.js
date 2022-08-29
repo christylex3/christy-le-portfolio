cd2f0f872b7581a6a02b093d069f4032bfbb94f9
Christy Le <christylex3@gmail.com>
2022-08-29 14:51:59 -0400

    Fixed navbar for 'about me' to work properly too!

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ const Header = () => {
         <header class="flex flex-col flex-wrap justify-between items-center z-10">
             <a href="/"><h1>Christy Le</h1></a>
             <nav class="flex flex-col sm:flex-row">
-                <a href="/">about me</a>
-                <a href="/work">work</a>
+                <a href="/christy-le-portfolio">about me</a>
+                <a href="/christy-le-portfolio/work">work</a>
                 <a href="#contact-info">contact</a>
                 {/* <!-- Resume is in progress, so placeholder is downloaded --> */}
                 <a href={placeholder} alt="Christy Le's Resume" download>resume</a>
