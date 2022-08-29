6c1685cfc9c6d57d4037feec60502981f5cc7224
Christy Le <christylex3@gmail.com>
2022-08-29 15:22:16 -0400

    Converted a tags in navBar to Links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import placeholder from "../images/resume_placeholder.jpg";
 
 const Header = () => {
@@ -7,8 +7,9 @@ const Header = () => {
         <header class="flex flex-col flex-wrap justify-between items-center z-10">
             <a href="/"><h1>Christy Le</h1></a>
             <nav class="flex flex-col sm:flex-row">
-                <a href="/christy-le-portfolio">about me</a>
-                <a href="/christy-le-portfolio/work">work</a>
+                <Link to="/christy-le-portfolio">about me</Link>
+                
+                <Link to="/christy-le-portfolio/work">work</Link>
                 <a href="#contact-info">contact</a>
                 {/* <!-- Resume is in progress, so placeholder is downloaded --> */}
                 <a href={placeholder} alt="Christy Le's Resume" download>resume</a>
