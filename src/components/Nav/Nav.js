113f0c8f98f24e11509484196e226483b9ee91fd
Christy Le <christylex3@gmail.com>
2023-06-29 17:10:03 -0400

    Matched mobile navbar with desktop navbar with button hover

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -96,7 +96,7 @@ function Nav() {
 								</a>
 							) : (
 								<a
-									className="text-amber-300 border border-amber-300 rounded-xl bg-transparent hover:bg-gradient-to-r hover:from-cyan-400 hover:to-indigo-400 hover:text-white hover:border-[#161616] py-2 px-5"
+									className="text-amber-300 border border-amber-300 rounded-xl bg-transparent hover:bg-gradient-to-r hover:from-amber-400 hover:to-orange-500 hover:text-white hover:border-[#161616] px-6 py-3 "
 									href={resume}
 									target="_blank"
 									rel="noreferrer"
